6912ab797ec3923b98151416ad5a9883484d4851
Asger Schultz <asger.s@protonmail.com>
2023-08-29 16:00:07 +0200

    Smooth loss and make loss plot more readable

--- frozone/plot/plot_train.py
+++ frozone/plot/plot_train.py
@@ -11,14 +11,21 @@ _save_folder = "train-plots"
 
 def plot_loss(loc: str, train_cfg: TrainConfig, train_results: TrainResults):
     with TT.profile("Plot loss"), plots.Figure(os.path.join(loc, _save_folder, "loss.png")):
-        plt.plot(train_results.train_loss_x, label="Train loss $X$")
-        plt.plot(train_results.train_loss_u, label="Train loss $U$")
-        plt.plot(train_results.train_loss, label="Train loss")
+        plt.plot(train_results.train_loss_x, color="grey", alpha=0.2)
+        plt.plot(train_results.train_loss_u, color="grey", alpha=0.2)
+        plt.plot(train_results.train_loss, color="grey", alpha=0.2)
+        plt.plot(*plots.moving_avg(train_results.train_loss_x, neighbors=12), label="Train loss $X$", color=plots.tab_colours[0], alpha=0.8)
+        plt.plot(*plots.moving_avg(train_results.train_loss_u, neighbors=12), label="Train loss $U$", color=plots.tab_colours[1], alpha=0.8)
+        plt.plot(*plots.moving_avg(train_results.train_loss, neighbors=12), label="Train loss", color=plots.tab_colours[2], alpha=0.8)
 
-        plt.plot(train_results.checkpoints, train_results.test_loss_x, label="Test loss $X$")
-        plt.plot(train_results.checkpoints, train_results.test_loss_u, label="Test loss $U$")
-        plt.plot(train_results.checkpoints, train_results.test_loss, label="Test loss")
+        plt.plot(train_results.checkpoints, train_results.test_loss_x, "-o", color="black", lw=2.7, ms=8)
+        plt.plot(train_results.checkpoints, train_results.test_loss_u, "-o", color="black", lw=2.7, ms=8)
+        plt.plot(train_results.checkpoints, train_results.test_loss, "-o", color="black", lw=2.7, ms=8)
+        plt.plot(train_results.checkpoints, train_results.test_loss_x, "-o", label="Test loss $X$", color=plots.tab_colours[0], lw=1.5, ms=6)
+        plt.plot(train_results.checkpoints, train_results.test_loss_u, "-o", label="Test loss $U$", color=plots.tab_colours[1], lw=1.5, ms=6)
+        plt.plot(train_results.checkpoints, train_results.test_loss, "-o", label="Test loss", color=plots.tab_colours[2], lw=1.5, ms=6)
 
+        plt.title(train_cfg.env)
         plt.xlabel("Batch")
         plt.ylabel("Loss")
         plt.yscale("log")
