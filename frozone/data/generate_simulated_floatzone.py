00d587f22ea47e51d745c146d93d2ff44cd931b3
asgerius <asger.s@protonmail.com>
2023-10-29 14:36:13 +0100

    Parallelize and more verbose logging

--- frozone/data/generate_simulated_floatzone.py
+++ frozone/data/generate_simulated_floatzone.py
@@ -1,3 +1,4 @@
+import multiprocessing as mp
 import os
 import shutil
 import warnings
@@ -15,7 +16,7 @@ from frozone.train import TrainConfig
 
 warnings.filterwarnings("error")
 
-def simulate(metadata: Metadata, X: np.ndarray, U: np.ndarray, S: np.ndarray) -> bool:
+def simulate(metadata: Metadata, X: np.ndarray, U: np.ndarray, S: np.ndarray) -> int:
 
     assert FloatZone.dt == Steuermann.dt
 
@@ -39,17 +40,17 @@ def simulate(metadata: Metadata, X: np.ndarray, U: np.ndarray, S: np.ndarray) ->
     U_st[:, Steuermann.ULabels.PolyPullRate]     = U[:, FloatZone.ULabels.PolyPullRate]
     U_st[:, Steuermann.ULabels.CrystalPullRate]  = U[:, FloatZone.ULabels.CrystalPullRate]
 
-    Z_st[:, Steuermann.ZLabels.Time] = Steuermann.dt * np.arange(metadata.length)
-    Z_st[:, Steuermann.ZLabels.MeltingRate] = U[:, Steuermann.ULabels.PolyPullRate] / 60
-    Z_st[:, Steuermann.ZLabels.CrystallizationRate] = U[:, Steuermann.ULabels.CrystalPullRate] / 60
-    Z_st[:, Steuermann.ZLabels.TdGeneratorVoltage] = U[:, Steuermann.ULabels.GeneratorVoltage]
+    Z_st[0, Steuermann.ZLabels.Time] = 0
+    Z_st[0, Steuermann.ZLabels.MeltingRate] = U[0, Steuermann.ULabels.PolyPullRate] / 60
+    Z_st[0, Steuermann.ZLabels.CrystallizationRate] = U[0, Steuermann.ULabels.CrystalPullRate] / 60
+    Z_st[0, Steuermann.ZLabels.TdGeneratorVoltage] = U[0, Steuermann.ULabels.GeneratorVoltage]
 
     for i in range(iters):
         try:
             X_st[[i+1]], _, Z_st[[i+1]] = Steuermann.forward(X_st[[i]], U_st[[i]], S[[i]], Z_st[[i]])
         except Exception as e:
             log.log_with_stacktrace(e, LogLevels.WARNING)
-            return False
+            return 1
         X_st[i+1, Steuermann.XLabels.PolyDia] = X[i+1, FloatZone.XLabels.PolyDia]
         X_st[i+1, Steuermann.XLabels.PolyAngle] = X[i+1, FloatZone.XLabels.PolyAngle]
 
@@ -65,7 +66,23 @@ def simulate(metadata: Metadata, X: np.ndarray, U: np.ndarray, S: np.ndarray) ->
     X[:, FloatZone.XLabels.PolyAngle] = X_st[:, Steuermann.XLabels.PolyAngle]
     X[:, FloatZone.XLabels.CrystalAngle] = X_st[:, Steuermann.XLabels.CrystalAngle]
 
-    return True
+    return 0
+
+def run_single_sim(args: tuple) -> int:
+    with log.collect:
+        (metadata, (X, U, S, R)), save_file = args
+        assert FloatZone.is_phase("Cone", S).all()
+        if not (X[:, FloatZone.XLabels.PolyDia] > 0).all():
+            log.warning("Code -1: %s" % metadata.raw_file)
+            return -1
+        result_code = simulate(metadata, X, U, S)
+        if result_code == 0:
+            save_loc = save_file.replace(f"/{PROCESSED_SUBDIR}/", f"/{SIMULATED_SUBDIR}/")
+            os.makedirs(os.path.split(save_loc)[0], exist_ok=True)
+            np.savez(save_loc, metadata=metadata, X=X, U=U, S=S, R=R)
+        else:
+            log.warning("Code %i: %s" % (result_code, metadata.raw_file))
+        return result_code
 
 def generate(data_path: str):
 
@@ -80,30 +97,25 @@ def generate(data_path: str):
     )
 
     npz_files = list_processed_data_files(data_path, ".")
-    log("Loading %i files" % len(npz_files))
+    log(f"Loading {len(npz_files):,} data files")
     dataset, npz_files = load_data_files(npz_files, tmp_train_cfg, max_num_files=tmp_train_cfg.max_num_data_files)
+    log(f"Loaded {len(dataset):,} data files")
 
-    for (metadata, (X, U, S, R)), save_file in zip(tqdm(dataset, disable=True), npz_files, strict=True):
-        assert FloatZone.is_phase("Cone", S).all()
-        if not (X[:, FloatZone.XLabels.PolyDia] > 0).all():
-            # TODO Dump these files, for they are probably indicative of mistakes
-            log.warning("0 poly diameter detected", metadata.raw_file)
-            continue
-        success = simulate(metadata, X, U, S)
-        if success:
-            save_loc = save_file.replace(f"/{PROCESSED_SUBDIR}/", f"/{SIMULATED_SUBDIR}/")
-            os.makedirs(os.path.split(save_loc)[0], exist_ok=True)
-            np.savez(save_loc, metadata=metadata, X=X, U=U, S=S, R=R)
-            log.debug("Simulation successfull", metadata.raw_file)
-        else:
-            log.warning("Simulation unsuccessfull", metadata.raw_file)
+    args = [(data, save_file) for data, save_file in zip(dataset, npz_files, strict=True)]
+    with mp.Pool() as pool:
+        results = tuple(tqdm(pool.imap(run_single_sim, args, chunksize=256), total=len(args)))
+
+    results = np.array(results)
+    log(
+        *(f"Code {i}: {(results==i).sum():,}" for i in np.unique(results))
+    )
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("data_path")
     args = parser.parse_args()
 
-    log.configure(os.path.join(args.data_path, "simulate.log"))
+    log.configure(os.path.join(args.data_path, "simulate.log"), print_level=None)
 
     with log.log_errors:
         generate(args.data_path)
