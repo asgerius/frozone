35049c86b1e0919d421841c6f3dbedaca243bad7
asgerius <asger.s@protonmail.com>
2023-10-29 14:37:43 +0100

    Remove unused plot function

--- frozone/environments/steuermann_model/framework.py
+++ frozone/environments/steuermann_model/framework.py
@@ -1,17 +1,11 @@
-"""
-Moduls for framework functions
-"""
-
-
 import numpy as np
-import matplotlib.pyplot as plt
 
 from frozone.environments.steuermann_model.model.system import System
 
 
 def rungekutta(tstart, tend, h, f, x, **kwargs):
     """
-    Calculates rungekutter from t start to t end.
+    Calculates runge kutta from t start to t end.
 
     :param tend:
     :param tstart:
@@ -41,7 +35,7 @@ def rungekutta(tstart, tend, h, f, x, **kwargs):
 
 def simulate(x, u, t0, t1, f, **kwargs) -> np.ndarray:
     """
-    :param x: initialstate
+    :param x: initial state
     :param f: system of differential equations
     :param u: insput sequence
     :return: Future state prediction
@@ -54,27 +48,3 @@ def simulate(x, u, t0, t1, f, **kwargs) -> np.ndarray:
     sys.set_within_limits(x_pred)
 
     return x_pred
-
-
-def plot(x, labels):
-    """Generate plot for N-1 Columns in matrix
-
-    :param labels:
-    :param x: Matrix where column is the x axis
-    """
-
-    print("start plotting")
-
-    time = list(np.arange(0, 60, 1/60)) + [60]
-
-    for i in range(x.shape[1]):
-        data = x[:, i]
-        plt.plot(time, data)
-        plt.grid(True, which='major', color='#666666', linestyle='-')
-        plt.minorticks_on()
-        plt.grid(True, which='minor', color='#999999', linestyle='-', alpha=0.2)
-        plt.xlabel("Time [s]")
-        plt.ylabel(labels[i])
-        plt.xlim(-2, float(max(time)))
-        plt.ylim(float(min(data))*0.99, float(max(data)*1.01))
-        plt.show()
